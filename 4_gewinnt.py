e3e18eb233cb2642ff12723c4cc454e85d4eb043
ka-fi12-sikbidi-rizz <12019727@edu.campus02.at>
2025-02-06 19:22:42 +0100

    Update 4_gewinnt.py
    updated player class

--- 4_gewinnt.py
+++ 4_gewinnt.py
@@ -1,30 +1,10 @@
 class Player:
 
-    """
-    sets symbol for a player
-    returns symbol of the player
-    sets name for a player
-    returns the name of a player
-    player can choose name and symbol, we have 21 coins
-    """
+    def set_number(self, number: int):
+        self.number = number
 
-    def set_symbol(self, symbol) -> None:
-        self.symbol = symbol
+    def get_number(self) -> int:
+        return self.get_number()
 
-    def get_symbol(self) -> str:
-        return self.symbol
-
-
-
-    def set_name(self, name) -> None:
-        self.name = name
-        
-
-    def get_name(self) -> str:
-        return self.name
-    
-
-    def __init__(self):
-        self.name = ""
-        self.symbol = ""
-        self.coins = 21
+    def place_coin(self, coin):
+        return self.place_coin
