26fd3b94a4d240c4ae5f605b125eb5ea581349b0
ka-fi12-sikbidi-rizz <12019727@edu.campus02.at>
2025-02-09 20:37:37 +0100

    updatet unit test

--- unittest.py
+++ unittest.py
@@ -4,19 +4,19 @@ from vier_gewinnt import game
 
 class TestConnect4Game(unittest.TestCase):
     def setUp(self):
-        """Wird vor jedem Test ausgeführt - initialisiert ein neues Spiel"""
+
         self.test_game = game()
         self.test_game.game_start()
 
     def test_initial_board_empty(self):
-        """Testet ob das Spielfeld zu Beginn leer ist"""
+
         gameboard = self.test_game.get_gameboard().get_board()
         for i in range(6):
             for j in range(7):
                 self.assertEqual(gameboard[i, j].get_player_on_field(), 0)
 
     def test_place_coin(self):
-        """Testet ob ein Spielstein korrekt platziert wird"""
+
 
         self.test_game.place_coin(1)
         gameboard = self.test_game.get_gameboard().get_board()
@@ -26,7 +26,7 @@ class TestConnect4Game(unittest.TestCase):
         self.assertEqual(gameboard[4, 0].get_player_on_field(), 0)
 
     def test_place_coin_stacking(self):
-        """Testet ob Steine korrekt gestapelt werden"""
+
 
         self.test_game.place_coin(1)
         self.test_game.place_coin(2)
@@ -37,7 +37,7 @@ class TestConnect4Game(unittest.TestCase):
         self.assertEqual(gameboard[4, 0].get_player_on_field(), 2)  # Darüber
 
     def test_vertical_win(self):
-        """Testet ob ein vertikaler Gewinn erkannt wird"""
+
 
         for _ in range(4):
             self.test_game.place_coin(1)
@@ -45,7 +45,7 @@ class TestConnect4Game(unittest.TestCase):
         self.assertEqual(self.test_game.check_win(), 1)
 
     def test_horizontal_win(self):
-        """Testet ob ein horizontaler Gewinn erkannt wird"""
+
         gameboard = self.test_game.get_gameboard().get_board()
 
 
@@ -55,7 +55,7 @@ class TestConnect4Game(unittest.TestCase):
         self.assertEqual(self.test_game.check_win(), 1)
 
     def test_diagonal_win(self):
-        """Testet ob ein diagonaler Gewinn erkannt wird"""
+
         gameboard = self.test_game.get_gameboard().get_board()
 
 
@@ -72,11 +72,11 @@ class TestConnect4Game(unittest.TestCase):
         self.assertEqual(self.test_game.check_win(), 1)
 
     def test_no_win(self):
-        """Testet ob bei leerem Spielfeld kein Gewinner erkannt wird"""
+
         self.assertEqual(self.test_game.check_win(), 0)
 
     def test_full_column(self):
-        """Testet ob eine volle Spalte korrekt erkannt wird"""
+
         gameboard = self.test_game.get_gameboard().get_board()
 
 
@@ -90,7 +90,7 @@ class TestConnect4Game(unittest.TestCase):
         self.assertEqual(gameboard[0, 0].get_player_on_field(), 1)
 
     def test_invalid_column(self):
-        """Testet ob ungültige Spalten korrekt behandelt werden"""
+
         initial_board = self.test_game.get_gameboard().get_board()
         gameboard_state = [[initial_board[i, j].get_player_on_field()
                             for j in range(7)] for i in range(6)]
