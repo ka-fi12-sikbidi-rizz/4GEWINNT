cb76449d8517bd5ccd174e294b608666f519a9bf
ka-fi12-sikbidi-rizz <12019727@edu.campus02.at>
2025-02-10 17:07:13 +0100

    updated unittests

--- testcases.py
+++ testcases.py
@@ -1,27 +1,25 @@
 import unittest
+from unittest.mock import patch
 from vier_gewinnt import game
-
 
 
 class Unittest4WINS(unittest.TestCase):
     """
     Tests for the Connect 4 game.
-    Checks if all game rules and moves work correctly.
     """
 
     def setUp(self):
         """
-        Creates a new game before each test.
+        Creates a new game before each test and sets it to 2-player mode.
         """
         self.game = game()
-        self.game.game_start()
+        self.game._game__is_single_player = False  # Direkte Änderung: Immer 2-Spieler-Modus
 
-    def test_place_coin(self):
+    @patch('builtins.input', side_effect=['1'])
+    def test_place_coin(self, mock_input):
         """
         Checks if a coin can be placed on the board.
-        Makes sure it falls to the bottom of the column.
         """
-
         self.game.place_coin(1)
 
         board = self.game.get_gameboard().get_board()
@@ -29,19 +27,16 @@ class Unittest4WINS(unittest.TestCase):
         bottom = board[5, 0].get_player_on_field()
         self.assertEqual(bottom, 1)
 
-
         above = board[4, 0].get_player_on_field()
         self.assertEqual(above, 0)
 
-    def test_vertical_win(self):
+    @patch('builtins.input', side_effect=['1', '1', '1', '1'])
+    def test_vertical_win(self, mock_input):
         """
         Checks if a player wins by placing 4 coins in a column.
-        Tests if the game spots this type of win.
         """
-
-        for i in range(4):
+        for _ in range(4):
             self.game.place_coin(1)
-
 
         winner = self.game.check_win()
         self.assertEqual(winner, 1)
@@ -49,14 +44,11 @@ class Unittest4WINS(unittest.TestCase):
     def test_horizontal_win(self):
         """
         Checks if a player wins by placing 4 coins in a row.
-        Tests if the game spots this type of win.
         """
-
         board = self.game.get_gameboard().get_board()
 
         for spalte in range(4):
             board[5, spalte].set_player_on_field(1)  # Spieler 1 legt eine Münze
-
 
         winner = self.game.check_win()
         self.assertEqual(winner, 1)
@@ -64,20 +56,13 @@ class Unittest4WINS(unittest.TestCase):
     def test_diagonal_win(self):
         """
         Checks if a player wins by placing 4 coins diagonally.
-        Tests if the game spots this type of win.
         """
-
         board = self.game.get_gameboard().get_board()
 
-
         positionen = [
-
             (5, 0, 2), (5, 1, 2), (5, 2, 2), (5, 3, 1),
-
             (4, 0, 2), (4, 1, 2), (4, 2, 1),
-
             (3, 0, 2), (3, 1, 1),
-
             (2, 0, 1)
         ]
 
@@ -86,6 +71,7 @@ class Unittest4WINS(unittest.TestCase):
 
         winner = self.game.check_win()
         self.assertEqual(winner, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
